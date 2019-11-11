12192baa723fb995e463efe708bbde91493c1460
zhushengjie <zhushengjie@hyperchain.cn>
2019-11-11 14:42:04 +0800

    jwt

--- initRouter/initRouter.go
+++ initRouter/initRouter.go
@@ -1,27 +1,20 @@
 package initRouter
 
 import (
-	"GinHello/handler/article"
+	"GinHello/handler/user"
+	"GinHello/middleware"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
+	"time"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+	router.GET("/", middleware.Auth(), func(context *gin.Context) {
+		context.JSON(http.StatusOK, time.Now().Unix())
+	})
+	router.GET("/login", user.CreateJwt)
+	router.POST("/register", user.Register)
 
-	articleRouter := router.Group("")
-	{
-		// 通过获取单篇文章
-		articleRouter.GET("/article/:id", article.GetOne)
-		// 获取所有文章
-		articleRouter.GET("/articles", article.GetAll)
-		// 添加一篇文章
-		articleRouter.POST("/article", article.Insert)
-		// 删除一篇文章
-		articleRouter.DELETE("/article/:id", article.DeleteOne)
-	}
-	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json")
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	return router
 }
