d32941d322a2f6ea9c24b1406e775ff9036c67b4
zhushengjie <zhushengjie@hyperchain.cn>
2019-11-11 12:00:40 +0800

    gorm

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-openapi/jsonreference v0.19.3 // indirect
 	github.com/go-openapi/spec v0.19.4 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
+	github.com/jinzhu/gorm v1.9.11
 	github.com/mailru/easyjson v0.7.0 // indirect
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.2.0
--- initDB/initDB.go
+++ initDB/initDB.go
@@ -1,16 +1,17 @@
 package initDB
+
 import (
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"log"
 )
-var Db *sql.DB
+
+var Db *gorm.DB
+
 func init() {
 	var err error
-	Db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/ginhello")
+	Db, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/ginhello")
 	if err != nil {
 		log.Panicln("err:", err.Error())
 	}
-	Db.SetMaxOpenConns(10)
-	Db.SetMaxIdleConns(10)
 }
